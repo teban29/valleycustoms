c491926d5d081659f2705b071eaab04fb442abf2
Esteban Cardona <ecardonagonzalez1029@gmail.com>
2024-12-13 20:12:36 -0500

    carpeta de pdf y ajuste al home

--- main/urls.py
+++ main/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # Ruta para la página principal
     path('quienes_somos/', views.quienes_somos, name='quienes_somos'),
 ]
+
+
--- valleycustoms/urls.py
+++ valleycustoms/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     
 ]
+
+
